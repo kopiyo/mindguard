400a0a1154145a3c596ebb2ee2c2b5ac382a5db9
kopiyo <97112892+kopiyo@users.noreply.github.com>
2026-04-21 03:35:30 +0300

    Update Try_streamlit_app_v2.py

--- Try_streamlit_app_v2.py
+++ Try_streamlit_app_v2.py
@@ -659,7 +659,7 @@ def load_model_and_tokenizer():
             num_labels=2,
             ignore_mismatched_sizes=True,)
         weights_path = hf_hub_download(repo_id=st.secrets["HF_REPO_ID"],filename="mindguard_best_weights.pt",token=st.secrets["HF_TOKEN"])
-state_dict = torch.load(weights_path, map_location=device)
+
         model.load_state_dict(state_dict)
         model = model.to(device)
         model.eval()
